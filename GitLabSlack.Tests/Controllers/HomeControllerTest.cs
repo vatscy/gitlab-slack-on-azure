6d5dbe5acf63b61ffcf9db334f33753c1b20e249
小林 正弥 <kobayashi_na@mti.co.jp>
2015-04-22 21:14:36 +0900

    create project

---
+++ GitLabSlack.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GitLabSlack;
+using GitLabSlack.Controllers;
+
+namespace GitLabSlack.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // 準備
+            HomeController controller = new HomeController();
+
+            // 実行
+            ViewResult result = controller.Index() as ViewResult;
+
+            // アサート
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ GitLabSlack/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GitLabSlack.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
